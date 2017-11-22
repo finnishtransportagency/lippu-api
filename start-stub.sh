75fccee905e5740674d6331c5a0776618c9f1c5c
Markku Korkeala <markku.korkeala@solita.fi>
2017-11-22 15:15:41 +0200

    Add departureTimeEarliest and arrivalTimeLatest to availability query
    Closes #6
    Add: departureTimeEarliest and arrivalTimeLatest to availability request
         and departureTime and arrivalTime in response.
    Del: dateTime from Travel, replaced by departureTimeEarliest and
         arrivalTimeLatest
    Add: TravelRequest and TravelResponse to have different time attributes
         in availability query
    Del: descriptions for optional {date}-param in products-query, make it
         required

--- start-stub.sh
+++ start-stub.sh
@@ -5,5 +5,10 @@ java -jar /codegen/swagger-codegen-cli.jar generate \
          -o /stubs
 
 cd stubs
+xmlstarlet ed -O -L \
+         -N pom=http://maven.apache.org/POM/4.0.0 \
+         -u "/pom:project/pom:properties/pom:version.framework" \
+         -v "1.0.0" pom.xml
+
 mvn package
 java -jar target/swagger-undertow-server-1.0.0.jar
